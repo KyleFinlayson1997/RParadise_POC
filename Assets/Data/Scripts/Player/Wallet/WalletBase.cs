06248bcdb2827abc728deb688acd6c4c0644cbb7
MeatballAlligator <kyledice322@gmail.com>
2019-04-21 15:31:18 +1000

    Made a wallet numberio appear on the screen

---
+++ Assets/Data/Scripts/Player/Wallet/WalletBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WalletBase : MonoBehaviour
+{
+    public float money = 100;
+
+    public Text moneyUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moneyUI = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyUI.text = "$" + money;
+        if (money < 0)
+        {
+            money = 0;
+        }
+    }
+}
